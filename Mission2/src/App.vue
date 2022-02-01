4d49e31d114fb3712350f122c2ded514effd0916
Aya <90342033+KutouLife@users.noreply.github.com>
2022-02-01 14:28:14 +0800

    init Options API
    cos Sample code do not use Options API

--- Mission2/src/App.vue
+++ Mission2/src/App.vue
@@ -1,15 +1,32 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+import HelloWorldVue from "/src/components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+};
 </script>
+
+<!--
+    <script setup>
+      // This starter template is using Vue 3 <script setup> SFCs
+      // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+      import HelloWorld from "/src/components/HelloWorld.vue";
+    </script>
+  -->
+
+
 
 <template>
   <section>
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <img alt="Vue logo" src="/src/assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + Vite" />
   </section>
 </template>
+
+
 
 <style>
 #app {
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Vue3-2022
+# Main-Vue3_2022
+
+- index
+  - <https://learn-at-hexschool.github.io/Main-Vue3-2022/>
+
+- Mission2
+  - <https://learn-at-hexschool.github.io/Main-Vue3-2022/Mission2/>
+  
